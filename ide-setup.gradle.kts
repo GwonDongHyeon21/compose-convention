979bd72c5211c89b06eddea759293ad592d177ab
GwonDongHyeon21 <GwonDH21@gmail.com>
2026-01-15 02:49:12 +0900

    chore: ide-setup.gradle 파일에 detekt.yml 관련 코드 추가

--- ide-setup.gradle.kts
+++ ide-setup.gradle.kts
@@ -1,21 +1,40 @@
-val updateDetektRules by tasks.registering(Copy::class) {
-    group = "verification"
-    description = "Downloads custom detekt rules for IDE integration"
+import java.net.URL
 
-    // 1. 'detektPlugins' 의존성 목록 중에서
-    // 2. 이름에 'compose-convention'이 포함된 파일(내 라이브러리)만 찾아서
+val updateDetektJar by tasks.registering(Copy::class) {
+    group = "verification"
     from(project.configurations.getByName("detektPlugins").files.filter {
         it.name.contains("compose-convention")
     })
-
-    // 3. 앱 프로젝트의 'config/detekt/rules' 폴더로 복사하고
     into(project.rootProject.file("config/detekt/rules"))
-
-    // 4. 파일 이름을 'custom-rules.jar'로 고정 (IDE 설정이 깨지지 않게)
     rename { "custom-rules.jar" }
 }
 
-// 5. 앱이 빌드되기 전에 이 작업을 무조건 먼저 실행 (자동화)
+val downloadDetektConfig by tasks.registering {
+    group = "verification"
+    description = "Downloads default detekt.yml if not exists"
+
+    val configDir = project.rootProject.file("config/detekt")
+    val configFile = File(configDir, "detekt.yml")
+
+    val rawUrl =
+        "https://raw.githubusercontent.com/GwonDongHyeon21/compose-convention/refs/heads/dev/detekt.yml"
+
+    doLast {
+        if (!configDir.exists()) {
+            configDir.mkdirs()
+        }
+
+        if (!configFile.exists()) {
+            runCatching {
+                configFile.writeBytes(URL(rawUrl).readBytes())
+            }.onFailure {
+                println("Failed to download detekt.yml: ${it.message}")
+            }
+        }
+    }
+}
+
 tasks.named("preBuild").configure {
-    dependsOn(updateDetektRules)
+    dependsOn(updateDetektJar)
+    dependsOn(downloadDetektConfig)
 }
