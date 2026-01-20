a668ace710769229c3bad8cb4549676f7a2aec43
GwonDongHyeon21 <GwonDH21@gmail.com>
2026-01-21 03:16:10 +0900

    chore: 버전 1.3.0 업데이트, ide-setup.gradle 파일 수정

--- ide-setup.gradle.kts
+++ ide-setup.gradle.kts
@@ -30,14 +30,19 @@ val setupDetekt by tasks.registering {
             println("[Detekt Setup] 새로운 버전 감지! ($savedVersion -> $currentVersion)")
             println("업데이트를 진행합니다...")
 
-            if (!detektRulesDir.exists()) detektRulesDir.mkdirs()
+            // 폴더가 없으면 생성
+            if (!detektRulesDir.exists()) {
+                detektRulesDir.mkdirs()
+            } else {
+                detektRulesDir.listFiles()?.forEach { it.delete() }
+            }
 
-            // JAR 파일 복사
-            val jarFile = artifact.file
-            val targetJar = detektRulesDir.resolve("custom-rules.jar")
+            // JAR 파일 복사 (원본 이름 그대로 사용)
+            val sourceJar = artifact.file
+            val targetJar = detektRulesDir.resolve(sourceJar.name)
 
             try {
-                jarFile.copyTo(targetJar, overwrite = true)
+                sourceJar.copyTo(targetJar, overwrite = true)
                 println("JAR 파일 업데이트 완료: ${targetJar.name}")
             } catch (e: Exception) {
                 println("JAR 복사 실패: ${e.message}")
@@ -56,7 +61,6 @@ val setupDetekt by tasks.registering {
 
             versionFile.writeText(currentVersion)
             println("Detekt 설정이 버전 $currentVersion (으)로 업데이트 되었습니다.\n")
-
         }
     }
 }
